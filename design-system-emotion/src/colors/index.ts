5e6e6fa09f90fd6a163b592a48f5b7cf446ac9cd
rldnrl <devpark1002@gmail.com>
2022-06-16 22:02:16 +0900

    feat: colors export default

--- design-system-emotion/src/colors/index.ts
+++ design-system-emotion/src/colors/index.ts
@@ -1,9 +1,23 @@
-export { default as blue } from './blue';
-export { default as lightBlue } from './light-blue';
-export { default as cyan } from './cyan';
-export { default as indigo } from './indigo';
-export { default as pink } from './pink';
-export { default as purple } from './purple';
-export { default as deepPurple } from './deep-purple';
-export { default as red } from './red';
-export { default as teal } from './teal';
+import blue from './blue';
+import lightBlue from './light-blue';
+import cyan from './cyan';
+import indigo from './indigo';
+import teal from './teal';
+import purple from './purple';
+import deepPurple from './deep-purple';
+import red from './red';
+import pink from './pink';
+
+const colors = {
+  blue,
+  lightBlue,
+  cyan,
+  indigo,
+  teal,
+  purple,
+  deepPurple,
+  red,
+  pink,
+} as const;
+
+export default colors;

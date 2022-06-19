13b3f13fc01d6ac9f053f6a6fd8d27557d443a29
rldnrl <devpark1002@gmail.com>
2022-06-19 20:15:29 +0900

    feat: set up font weight

---
+++ design-system-emotion/src/typography/font-weight.ts
@@ -0,0 +1,13 @@
+const fontWeight = {
+  hairline: 100,
+  thin: 200,
+  light: 300,
+  regular: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+  extrabold: 800,
+  black: 900,
+} as const
+
+export default fontWeight
